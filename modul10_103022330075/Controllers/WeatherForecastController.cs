f30aa3922c0e549c800ccb33524b76051bf4a24a
unknown <raihanananda1980@gmail.com>
2025-04-29 08:12:05 +0700

    menambahkan API movie

--- modul10_103022330075/Controllers/WeatherForecastController.cs
+++ modul10_103022330075/Controllers/WeatherForecastController.cs
@@ -4,30 +4,61 @@ namespace modul10_103022330075.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class MovieController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static List<string> Stars1 = new List<string>
+       {
+           new string("Tim Robbins"),
+           new string("Morgan Freeman"),
+           new string("Bob Gunton")
+       };
+        private static List<Movie> MovieList = new List<Movie>
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            new Movie("The Shawshank Redemption","Frank Darabont",Stars1,"A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.")
+            , new Movie("The Godfather","Francis Ford Coppola",Stars2,"The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.")
+        ,new Movie("The Dark Knight","Christopher Nolana",Stars3,"When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private static List<string> Stars2 = new List<string>
         {
-            _logger = logger;
+            new string("Marlon Brando"),
+            new string("Al Pacino"),
+            new string("James Caan")
+
+        };
+
+
+        private static List<string> Stars3 = new List<string>
+        {
+            new string("Christian Bale"),
+            new string("Heath Ledger"),
+            new string("Aaron Eckhart")
+        };
+
+        [HttpGet]
+        public IEnumerable <Movie> Get()
+        {
+            return MovieList;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("id")]
+        public Movie Get(int id)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return MovieList[id];
         }
+
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            MovieList.Add(movie);   
+        }
+
+        [HttpDelete("id")]
+        public void Delete(int id)
+        {
+            MovieList.RemoveAt(id);
+        }
+       
+        
     }
 }
---
+++ modul10_103022330075/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace modul10_103022330075
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+        public Movie(string title, string director, List<string> stars, string description)
+        {
+            Title = title;
+            Director = director;
+            Stars = stars;
+            Description = description;
+        }
+
+    }
+    
+}
